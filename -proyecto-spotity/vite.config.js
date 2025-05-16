1f5e54b3a52ab488a3a544dc68aa2924c5aa6e63
Mariana Arango <74248778+marianaArangoE@users.noreply.github.com>
2025-05-16 00:26:34 -0500

    Update vite.config.js

--- -proyecto-spotity/vite.config.js
+++ -proyecto-spotity/vite.config.js
@@ -2,5 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+    cors: true,
+    allowedHosts: [
+      'miapp.loca.lt',
+      'clonspotify.onrender.com'
+    ],
+    hmr: {
+      protocol: 'wss',
+      host: 'miapp.loca.lt',
+      clientPort: 443
+    },
+  },
 });
